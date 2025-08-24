707a12ea773d0681e256b01cc066225bfbb8310c
cortexinvader <cortexinvader@gmail.com>
2025-08-24 16:37:14 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-require("../bot/index.js"); 
+import "../bot/index.js";
 
 const app = express();
 app.use(express.json());
